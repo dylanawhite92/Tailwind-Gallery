f3484e24ee1d91daf4627831165411f8b46463da
The Crystal King <dylanawhite11@gmail.com>
2021-04-13 13:13:39 -0500

    adding card stats, hardcoding for style testing before implementing pixabay

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ function App() {
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by John Doe
         </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            300
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            420
+          </li>
+        </ul>
       </div>
     </div>
   );
